50ad85228d343c08a116b6e240d8f95403a04414
Javier Castillo <javiercame98@gmail.com>
2025-12-09 17:36:10 +0100

    feat: printing with lambda

---
+++ src/S0813/MonthList.java
@@ -0,0 +1,29 @@
+package S0813;
+
+import java.util.List;
+
+public class MonthList {
+
+    public static void main(String[] args) {
+
+        List<String> MonthsList = List.of("January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+        );
+
+        MonthsList.forEach(System.out::println);
+    }
+
+
+
+
+}
