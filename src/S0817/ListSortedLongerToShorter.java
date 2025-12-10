510bbe897e47c08dd0677faf1e165e7aeb3988a0
Javier Castillo <javiercame98@gmail.com>
2025-12-10 12:20:53 +0100

    feat: creating a sortedList with stream, sorted and collect (longer string to shoter string)

---
+++ src/S0817/ListSortedLongerToShorter.java
@@ -0,0 +1,39 @@
+package S0817;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ListSortedLongerToShorter {
+    public static void main(String[] args) {
+
+        List<Object> mixtList = new ArrayList<>();
+        mixtList.add("Hola");
+        mixtList.add(1);
+        mixtList.add("Macarron");
+        mixtList.add(34);
+        mixtList.add("Salamandra");
+        mixtList.add(123456789);
+
+
+
+        List<Object> sortedList = mixtList.stream().sorted(Comparator.comparingInt(
+                item -> {
+                    String allToString;
+                    if(item instanceof String){
+                        allToString = (String) item;
+                    }else if(item instanceof Integer){
+                        allToString = String.valueOf(item);
+                    }else{
+                        return 0;
+                    }
+                    return allToString.length();
+                }
+        ).reversed()).collect(Collectors.toList());
+
+
+        System.out.println(sortedList);
+
+    }
+}
